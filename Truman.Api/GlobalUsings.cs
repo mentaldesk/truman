a6b8fb42d87cf8e13ebf91bdbb56b61f3a5631b4
James Crosswell <jamescrosswell@users.noreply.github.com>
2025-07-04 22:18:57 +1200

    Fixed ports

--- Truman.Api/GlobalUsings.cs
+++ Truman.Api/GlobalUsings.cs
@@ -8,4 +8,5 @@ global using Microsoft.IdentityModel.Tokens;
 global using System.IdentityModel.Tokens.Jwt;
 global using System.Security.Claims;
 global using System.Text;
+global using Microsoft.Extensions.Options;
 global using Truman.Api.Features.Auth; 
