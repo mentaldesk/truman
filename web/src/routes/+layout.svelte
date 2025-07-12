f6f57b25c2de1dc37c34148d69a6d4c0c196d812
James Crosswell <jamescrosswell@users.noreply.github.com>
2025-07-12 13:40:45 +1200

    Update +layout.svelte

--- web/src/routes/+layout.svelte
+++ web/src/routes/+layout.svelte
@@ -7,16 +7,12 @@
 </script>
 
 <style>
-	.horizon-bar {
-		width: 100%;
-		height: 120px;
+	:global(html, body) {
 		background-image: url('/images/horizon.jpg');
 		background-repeat: repeat-x;
 		background-position: top center;
 		background-size: auto 100%;
 		background-attachment: fixed;
-	}
-	:global(html, body) {
 		height: 100%;
 		margin: 0;
 		padding: 0;
@@ -28,7 +24,7 @@
 		<div class="animate-spin rounded-full h-12 w-12 border-t-2 border-b-2 border-blue-500"></div>
 	</div>
 {:else}
-	<div class="min-h-screen horizon-bar">
+	<div class="min-h-screen">
 		{@render children()}
 	</div>
 {/if}
