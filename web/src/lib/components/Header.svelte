1bbb8b0fcc1c86392c7e8122a815088ec0e8bd65
James Crosswell <jamescrosswell@users.noreply.github.com>
2025-07-12 22:32:02 +1200

    Added door to header

--- web/src/lib/components/Header.svelte
+++ web/src/lib/components/Header.svelte
@@ -60,10 +60,13 @@
 <div class="header-bg bg-gray-100/50 w-full">
   <header class="p-4 border-b border-gray-200">
     <div class="max-w-7xl mx-auto flex items-center justify-between">
-      <div class="flex-1 flex items-center">
+      <div class="flex flex-1 items-center">
         <a href="/" class="flex-shrink-0">
           <h1 class="text-2xl font-bold text-gray-900">TRUMAN.NEWS</h1>
         </a>
+      </div>
+      <div class="flex flex-2 items-end -mb-4">
+          <img src="/images/door.png" alt="Door" class="mx-8 h-[72px] w-auto object-contain self-end" style="max-height:72px;" />
       </div>
       <div class="flex items-center space-x-6">
         <!-- Mood Slider -->
