23da7568c68d7e449def8ab734c6ad8e6ea0a031
James Crosswell <jamescrosswell@users.noreply.github.com>
2025-09-24 16:15:04 +1200

    Added github actions to push docker images to GHCR

--- src/web/entrypoint.sh
+++ src/web/entrypoint.sh
@@ -4,10 +4,16 @@
 : "${VITE_ENVIRONMENT:=production}"
 : "${Sentry__Dsn:=}"
 
+VERSION="dev"
+if [ -f /usr/share/nginx/html/version.txt ]; then
+  VERSION=$(cat /usr/share/nginx/html/version.txt)
+fi
+
 cat > /usr/share/nginx/html/config.js << EOF
 window.__API_URL__ = "${VITE_API_URL}";
 window.__ENVIRONMENT__ = "${VITE_ENVIRONMENT}";
 window.__SENTRY_DSN__ = "${Sentry__Dsn}";
+window.__VERSION__ = "${VERSION}";
 EOF
 
-exec nginx -g 'daemon off;' 
+exec nginx -g 'daemon off;'
